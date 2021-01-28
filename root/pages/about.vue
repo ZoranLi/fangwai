64829740aa60d06158d3272208937fb80e254079
zoranlee <lizhangliao@aixhc.com>
2021-01-28 10:44:32 +0800

    修改名称

--- root/pages/about.vue
+++ root/pages/about.vue
@@ -1,26 +1,35 @@
 <template>
-	<div>
-		<h1>About</h1>
-		<p>Duis exercitation id ullamco elit officia labore ex velit adipisicing cillum adipisicing.</p>
-		<a href="https://github.com/TiagoDanin/Nuxt-SEO/">GitHub project</a>
-		<br>
-		<p>Links:</p>
-		<ul>
-			<li><nuxt-link to="/">Index</nuxt-link></li>
-			<li><nuxt-link to="/news">News</nuxt-link></li>
-			<li><nuxt-link to="/news?query=true">News with query</nuxt-link></li>
-			<li><nuxt-link to="/login?query=true">Login with query and canonical with trailing slash forced</nuxt-link></li>
-			<li><nuxt-link to="/about">About</nuxt-link></li>
-		</ul>
-	</div>
+    <div>
+        <h1>About</h1>
+        <p>Duis exercitation id ullamco elit officia labore ex velit adipisicing cillum adipisicing.</p>
+        <br>
+        <p>Links:</p>
+        <ul>
+            <li>
+                <nuxt-link to="/">Index</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/news">News</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/news?query=true">News with query</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/login?query=true">Login with query and canonical with trailing slash forced</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/about">About</nuxt-link>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-	export default {
-		head: function() {
-			return this.$seo({
-				title: 'About'
-			})
-		}
-	}
+    export default {
+        head: function () {
+            return this.$seo({
+                title: 'About'
+            })
+        }
+    }
 </script>
